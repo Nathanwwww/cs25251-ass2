600214586c9ea1bdf8e8d62f607e0dab3fc673b1
Nathan1081 <zhounan2155@gmail.com>
2018-05-21 14:56:38 +1000

    Part1

---
+++ Graph.c
@@ -0,0 +1,13 @@
+//cs2521 ass
+
+//===========================================================================
+//function 
+Graph newGraph(int noNodes);
+void  insertEdge(Graph g, Vertex src, Vertex dest, int weight);
+void  removeEdge(Graph g, Vertex src, Vertex dest);
+bool  adjacent(Graph g, Vertex src, Vertex dest);
+int  numVerticies(Graph g);
+//===========================================================================
+void insertEdge(Graph g, Vertax src, Vertax dest, int weight){
+    
+}
